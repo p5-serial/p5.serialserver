65f183406b939eb2d25612ade0f266a6a7dc2f26
PPEOL <45401452+PPEOL@users.noreply.github.com>
2019-05-01 16:51:30 +0100

    Update p5.serialserver.js
    serialPort.isOpen is not a function,
    as serialPort might have a string from ws, also check whether it is an object

--- p5.serialserver.js
+++ p5.serialserver.js
@@ -27,7 +27,7 @@ var start = function () {
 	var openSerial = function(serialport, serialoptions) {
 		logit("openSerial: " + serialport);
 
-		if (serialPort == null || serialPort.isOpen() == false) {
+		if (serialPort == null || (typeof serialPort === "object" && serialPort.isOpen == false)) {
 			logit("serialPort == null || !serialPort.isOpen()");
 
 			serialPortName = serialport;
@@ -92,7 +92,7 @@ var start = function () {
 
 	var closeSerial = function() {
 		logit("closeSerial");
-		if (serialPort != null && serialPort.isOpen()) {
+		if (serialPort != null && typeof serialPort === "object" && serialPort.isOpen == false) {
 			logit("serialPort != null && serialPort.isOpen so close");
 			logit("serialPort.flush, drain, close");
 
@@ -110,7 +110,7 @@ var start = function () {
 		}
 
 		// Let's try to close a different way
-		if (serialPort != null && serialPort.isOpen()) {
+		if (serialPort != null && typeof serialPort === "object" && serialPort.isOpen == false) {
 			logit("serialPort != null && serialPort.isOpen() is true so serialPort = null");
 
 			serialPort = null;
