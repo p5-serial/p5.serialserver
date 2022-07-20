26d187fafc424fd8bb4a8fbc5dda21dc0a4c15a8
aarón montoya-moraga <montoyamoraga@gmail.com>
2022-07-20 17:27:37 -0400

    updated twoPortReadexample to match new examples style

--- examples/11-twoPortRead/sketch.js
+++ examples/11-twoPortRead/sketch.js
@@ -7,36 +7,36 @@
   
 */
 
-var serial1 = new p5.SerialPort();
-var serial2 = new p5.SerialPort();
-var firstPort = '/dev/cu.usbmodem14111';
-var secondPort = '/dev/cu.usbmodem141431';
-var input1 = '';
-var input2 = '';
+let serial1 = new p5.SerialPort();
+let serial2 = new p5.SerialPort();
+let firstPort = "/dev/cu.usbmodem14111";
+let secondPort = "/dev/cu.usbmodem141431";
+let input1 = "";
+let input2 = "";
 
 function setup() {
   createCanvas(400, 300);
-  serial1.on('data', serialEvent);
-  serial1.on('error', serialError);
-  serial2.on('data', serial2Event);
-  serial2.on('error', serial2Error);
-  
+  serial1.on("data", serialEvent);
+  serial1.on("error", serialError);
+  serial2.on("data", serial2Event);
+  serial2.on("error", serial2Error);
+
   serial1.open(firstPort);
   serial2.open(secondPort);
 }
 
 function draw() {
-  background(0x23, 0x76, 0xF3);
+  background(0x23, 0x76, 0xf3);
   fill(255);
   text("data from serial port 1:" + input1, 30, 30);
   text("data from serial port 2: " + input2, 30, 90);
 }
 
-function serialEvent(){
-  data = serial1.readStringUntil('\r\n');
-  if (data.length > 0){
-      input1 = data;
-    }
+function serialEvent() {
+  data = serial1.readStringUntil("\r\n");
+  if (data.length > 0) {
+    input1 = data;
+  }
 }
 
 function serialError(err) {
@@ -44,13 +44,12 @@ function serialError(err) {
 }
 
 function serial2Event() {
-    var data = serial2.readStringUntil('\r\n');
-    if (data.length > 0){
-      input2 = data;
-    }
+  var data = serial2.readStringUntil("\r\n");
+  if (data.length > 0) {
+    input2 = data;
+  }
 }
 
 function serial2Error(err) {
-    println("error with serial port 2: " + err);
-
+  println("error with serial port 2: " + err);
 }
