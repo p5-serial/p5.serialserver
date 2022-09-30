ad9b469d348c3b808f21390742b583dcbf93df5f
aarón montoya-moraga <montoyamoraga@gmail.com>
2022-09-29 22:43:07 -0300

    make comments shorter

--- src/Client.js
+++ src/Client.js
@@ -1,33 +1,14 @@
-/**
- * @fileOverview Client object that gets created when new web socket client connects to server. Maintains SerialPort objects that the client subscribes to.
- *
- * @author Shawn Van Every
- * @author Jiwon Shin
- *
- * @requires NPM:serialport
- * @requires NPM:ws
- * @requires classes/SerialPort.js:SerialPort
- * */
+// Client object that gets created when new web socket client connects to server.
+// Maintains SerialPort objects that the client subscribes to.
 
 let sp = require('serialport');
 let SerialPort = require('./SerialPort');
-
 let WebSocketServer = require('ws').Server;
 
-/**
- * Represents a web socket client. Maintains {@link SerialPort SerialPort} objects that the client subscribes to.
- */
+// represents a web socket client. Maintains SerialPort objects that the client subscribes to.
 class Client {
-  /**
-   * create a web socket client
-   * @constructor
-   * @param {ws} ws - Web socket object
-   * @property {SerialPort[]} serialPorts - list of subscribed {@link SerialPort SerialPort} objects
-   * @property {string[]} serialPortsList - list of string names of subscribed {@link SerialPort SerialPort} objects. Used for checking whether duplicate serial port is requested to open.
-   */
   constructor(ws) {
     this.LOGGING = true;
-
     //websocket object for this client
     this.ws = ws;
     //list of serial ports that this client has opened
@@ -35,66 +16,40 @@ class Client {
     this.serialPortsList = [];
   }
 
-  /** echo received message back to web client */
+  // echo received message back to web client
   echo(msg) {
     this.sendit({ method: 'echo', data: msg });
   }
 
-  /** list all available serial ports and send it to the client*/
+  // list all available serial ports and send it to the client
   list() {
     let self = this;
 
     sp.list().then((ports) => {
       let portNames = [];
       ports.forEach(function (port) {
-        // according to https://serialport.io/docs/guide-upgrade
-        // comName has been renamed path, so this line
-        // portNames.push(port.comName);
-        // should now be this
         portNames.push(port.path);
         console.log(port.path);
       });
       self.sendit({ method: 'list', data: portNames });
-      
     });
-
-    // // this line broke with the upgrade from SerialPort 7.x to 8.x
-    // sp.list(function (err, ports) {
-    //   let portNames = [];
-    //   ports.forEach(function (port) {
-    //     // according to https://serialport.io/docs/guide-upgrade
-    //     // comName has been renamed path, so this line
-    //     // portNames.push(port.comName);
-    //     // should now be this
-    //     portNames.push(port.path);
-    //   });
-    //   self.sendit({ method: 'list', data: portNames });
-    // });
   }
 
-  /**
-   * add opened SerialPort object and its name
-   * @param {SerialPort} port - SerialPort object opened by client
-   * */
+  // add opened SerialPort object and its name
   openSerial(port) {
     this.serialPortsList.push(port.serialPortName);
     this.serialPorts.push(port);
-    console.log("open");
+    console.log('open');
   }
 
-  /**
-   * write received data to subscribed serial ports.
-   * @param {String} msg - received string data from client
-   * */
+  // write received data to subscribed serial ports
   write(msg) {
     for (let i = 0; i < this.serialPorts.length; i++) {
       this.serialPorts[i].serialPort.write(msg);
     }
   }
 
-  /**
-   * close client connection. Set serialPorts and serialPortsList array to null.
-   */
+  // close client connection. Set serialPorts and serialPortsList array to nulls
   close() {
     //this needs to be updated per port
     //also with client-side library
@@ -106,20 +61,14 @@ class Client {
     //this.sendit()
   }
 
-  /**
-   * console.log log messages when LOGGING == true
-   * @function logit
-   * @param {String} mess - String to log when LOGGING == true*/
+  // console.log log messages when LOGGING == true
   logit(mess) {
     if (this.LOGGING) {
       console.log(mess);
     }
   }
 
-  /**
-   * send data via websocket to the client
-   * @param {Object} toSend - JSON object received to be sent. Contains message method and data.
-   */
+  // send data via websocket to the client
   sendit(toSend) {
     let dataToSend = JSON.stringify(toSend);
 
